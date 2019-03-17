ee84d4ec718dd2cecbd433cfa0667448cfbfc9e8
Keunbae Park <ppotpo@gmail.com>
2019-03-17 10:17:29 -0400

    wip

--- mockery.php
+++ mockery.php
@@ -2,10 +2,10 @@
 
 use Mockery as m;
 
-m::mock(Some::class);
+$mock = m::mock(Some::class);
 
-$some->shouldReceive('some')->once()->andReturn(1);
-$some->shouldReceive('some')->with('foo', m::type(Some::class))->andReturnNull();
-$some->shouldReceive('some')->andReturnUsing(function ($array = []) {
+$mock->shouldReceive('some')->once()->andReturn(1);
+$mock->shouldReceive('some')->with('foo', m::type(Some::class))->andReturnNull();
+$mock->shouldReceive('some')->andReturnUsing(function ($array = []) {
     return new Some($array);
 });
