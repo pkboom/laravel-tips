1f3401786e77ba8ebf7685989c882a7240f0e726
Keunbae Park <ppotpo@gmail.com>
2019-03-19 12:00:08 -0400

    wip

--- mockery.php
+++ mockery.php
@@ -5,6 +5,7 @@ use Mockery as m;
 $mock = m::mock(Some::class);
 
 $mock->shouldReceive('some')->once()->andReturn(1);
+$mock->shouldReceive('some')->once()->andReturnSelf();
 $mock->shouldReceive('some')->with('foo', m::type(Some::class))->andReturnNull();
 $mock->shouldReceive('some')->andReturnUsing(function ($array = []) {
     return new Some($array);
--- phpunit.php
+++ phpunit.php
@@ -2,7 +2,7 @@
 
 // phpunit
 $some = $this->getMockBuilder(Some::class)->setConstructorArgs([$some, 1, null])->getMock();
-$some->expects($this->once())->method('some')->will($this->returnValue(1));
+$some->expects($this->once())->method('some')->with('some', 1)->will($this->returnValue(1));
 $some->expects($this->never())->method('some');
 
 $some = $this->getMockBuilder(Some::class)->getMock();
@@ -14,14 +14,24 @@ $some = $this->getMockBuilder(Some::class)->setMethods(null)->getMock();
 // all mocks, run the actual code, not overridable
 
 $some = $this->getMockBuilder(Some::class)->setMethods(['some'])->getMock();
-// The methods you have identified
-// all stubs, return null, overridable
-
-// Methods you did not identify
-// all mocks, run the actual code, not overridable
+// Methods you identified: all stubs, return null, overridable
+// Methods you didn't identify: all mocks, run the actual code, not overridable
 
 
 /**
  * @expectedException \LogicException
  * @expectedExceptionMessage Queueing collections with multiple model types is not supported.
  */
+
+ class SomeTest
+ {
+     public function setUp()
+     {
+         $this->p = new Some(1, 2);
+     }
+     
+     public function tearDown()
+     {
+         unset($this->p);
+     }
+ }
