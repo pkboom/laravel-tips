1bbef24b08f2663724bcc30149c748c3e4af01b2
Keunbae Park <ppotpo@gmail.com>
2019-06-25 10:45:34 -0400

    wip

--- error_blade_directive.php
+++ error_blade_directive.php
@@ -1,7 +1,5 @@
 <?php
 
-// Usage:
-
 // Before
 @if ($errors->has('email'))
     <span>{{ $errors->first('email') }}</span>
@@ -40,3 +38,29 @@ EOT;
         });
     }
 }
+
+////////////////////////////////////////////////////////////////////////////
+
+Blade::directive('error_class', function($expression) {
+    $args = explode(', ', $expression);
+
+    if (count($args) > 1) {
+        $args[1] = str_replace("'", '', args[1]);
+    } else {
+        $args[1] = 'border-red-500';
+    }
+
+    return '
+        <?php if ($errors->has('.$args[0].')) : ?>
+            <?php echo echo "'.$args[1].'"; ?>
+        <?php endif; ?>
+    ';
+});
+
+<input type="text" class="@error_class('name') w-full block">
+
+@error('name')
+    <p class="mt-3 text-red-500 text-sm font-bold">{{ $message }}</p>
+@enderror
+
+@error_class('name', 'form-danger')
