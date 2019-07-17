02292205f4ec5ec1397616e2675001c2f038cd1e
Keunbae Park <ppotpo@gmail.com>
2019-07-17 09:53:35 -0400

    wip

--- eloquent_builder_macros.php
+++ eloquent_builder_macros.php
@@ -11,6 +11,11 @@ class AppServiceProvider
         });
         
         // postgreSql: order by null first/last
+        // other sql: ORDER BY birth_date IS NOT NULL, birth_date DESC
+        // nulls last + desc = IS NOT NULL, DESC
+        // nulls last + asc = IS NULL, ASC
+        // nulls first + desc = IS NULL, DESC
+        // nulls first + asc = IS NOT NULL, ASC
         Builder::macro('orderByNulls', function ($column, $direction = 'asc', $nulls = 'last', $bindings = []) {
             $column = $this->getGrammar()->wrap($column);
             $direction = strtolower($direction) === 'asc' ? 'asc' : 'desc';
