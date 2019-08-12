18f469b0a4f5c506aae3d08f956a032917bd711d
Keunbae Park <ppotpo@gmail.com>
2019-08-12 12:09:50 -0400

    wip

--- Eloquent/eloquent_builder_macros.php
+++ Eloquent/eloquent_builder_macros.php
@@ -10,8 +10,8 @@ class AppServiceProvider
             return $this->whereDate('created_at', '<', $date);
         });
         
-        // postgreSql: order by null first/last
-        // other sql: ORDER BY birth_date IS NOT NULL, birth_date DESC
+        // postgreSql: order by some_column null first/last
+        // other sql: ORDER BY some_column IS NOT NULL, birth_date DESC
         // nulls last + desc = IS NOT NULL, DESC
         // nulls last + asc = IS NULL, ASC
         // nulls first + desc = IS NULL, DESC
@@ -30,6 +30,34 @@ class AppServiceProvider
     
         Builder::macro('orderByNullsFirstr', function ($column, $direction = 'asc', $bindings = []) {
             return $this->orderByNulls($column, $direction, 'first', $bindings);
+        });
+
+        Builder::macro('addSubSelect', function ($column, $query) {
+            if (is_null($this->columns)) {
+                $this->select($this->from.'.*');
+            }
+            return $this->selectSub($query->limit(1), $column);
+        });
+
+        Builder::macro('orderBySub', function ($query, $direction = 'asc', $nullPosition = null) {
+            if (!in_array($direction, ['asc', 'desc'])) {
+                throw new Exception('Not a valid direction.');
+            }
+            if (!in_array($nullPosition, [null, 'first', 'last'], true)) {
+                throw new Exception('Not a valid null position.');
+            }
+            return $this->orderByRaw(
+                implode('', ['(', $query->limit(1)->toSql(), ') ', $direction, $nullPosition ? ' NULLS '.strtoupper($nullPosition) : null]),
+                $query->getBindings()
+            );
+        });
+
+        Builder::macro('orderBySubAsc', function ($query, $nullPosition = null) {
+            return $this->orderBySub($query, 'asc', $nullPosition);
+        });
+
+        Builder::macro('orderBySubDesc', function ($query, $nullPosition = null) {
+            return $this->orderBySub($query, 'desc', $nullPosition);
         });
     }
 }
