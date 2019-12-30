b423ec0001082e37581512c3fe42d79464860233
Keunbae Park <ppotpo@gmail.com>
2019-12-29 23:09:37 -0500

    wip

---
+++ command.php
@@ -0,0 +1,9 @@
+<? php
+
+/**
+ * @link https://twitter.com/freekmurze/status/1191637214096166912
+ */
+
+// - if the command name is clear enough you don't need a description
+// - you don't need a constructor, everything used in `handle` will be resolved from the ioc
+// - the default comments are redundant
