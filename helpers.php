b5270b8dcf2433e69d64e437f0b94251ac6b79bb
Keunbae Park <ppotpo@gmail.com>
2020-08-06 14:39:55 -0400

    wip

--- helpers.php
+++
@@ -1,8 +0,0 @@
-<? php
-
-if (! function_exists('fail_validation')) {
-    function fail_validation(string $key, string $message)
-    {
-        throw ValidationException::withMessages([$key => $message]);
-    }
-}
